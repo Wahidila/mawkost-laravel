56f27c957c80ab568e28c19183e0387540e40257
Wahidila SP <contentsaya1@gmail.com>
2026-04-18 18:00:15 +0700

    fix: remove manual Intervention Image registration to fix 500 error
    Auto-discovery handles provider/facade registration automatically.
    Manual registration caused 500 error on all pages when package
    was not yet installed via composer on production server.

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -183,7 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Image' => Intervention\Image\Facades\Image::class,
+        // 'Image' alias auto-discovered by intervention/image package
     ])->toArray(),
 
 ];
